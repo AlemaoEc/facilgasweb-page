fd1aa02f397356dfe09ff3fcdd5ba1fe86ec2250
Diogo Soares <diogo.ec.2013@gmail.com>
2016-06-08 14:23:41 -0400

    Atualizando pagina

--- serviceworker.js
+++ serviceworker.js
@@ -24,14 +24,8 @@ self.addEventListener('push', function(event) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('Notification click: tag', event.notification.tag);
-  // Android doesn't close the notification when you click it
-  // See http://crbug.com/463146
   event.notification.close();
   var url = 'https://youtu.be/gYMkEMCHtJ4';
-  // Check if there's already a tab open with this URL.
-  // If yes: focus on the tab.
-  // If no: open a tab with the URL.
   event.waitUntil(
     clients.matchAll({
       type: 'window'
